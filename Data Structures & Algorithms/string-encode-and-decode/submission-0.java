4da49ee440f6a29131c1260d63dbba1c27af24ed
Noam Yan <noamyan9@gmail.com>
2026-04-04 19:43:10 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String str:strs){
+            sb.append(str.length());
+            sb.append("_");
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        while(i<str.length()){
+            while(Character.isDigit(str.charAt(i))){
+                sb.append(str.charAt(i));
+                i++;
+                continue;
+            }
+            int len = Integer.parseInt(sb.toString());
+            i++; // skip delimitor
+            sb = new StringBuilder();
+            while(len>0){
+                sb.append(str.charAt(i));
+                len--;
+                i++;
+            }
+            res.add(sb.toString());
+            sb = new StringBuilder();
+        }
+        return res;
+    }
+}
